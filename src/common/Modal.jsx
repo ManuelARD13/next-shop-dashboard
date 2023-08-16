c72e4a43dd002a35cc731d9cfa2ae57d065b5095
swlman <duranalejandro661@gmail.com>
2023-08-16 20:06:42 -0300

    Adding Modal to create products on the dashboard/product section

--- src/common/Modal.jsx
+++ src/common/Modal.jsx
@@ -1,12 +1,15 @@
 import { Fragment, useRef } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { XCircleIcon } from '@heroicons/react/solid';
+import { XCircleIcon } from '@heroicons/react/24/solid';
 
 export default function Modal({ open, setOpen, children }) {
+  console.log(open)
+
   const cancelButtonRef = useRef(null);
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpen}>
+    <>
+   <Transition.Root show={true} as={Fragment}>
+      <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={() => setOpen()}>
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
             <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
@@ -35,5 +38,6 @@ export default function Modal({ open, setOpen, children }) {
         </div>
       </Dialog>
     </Transition.Root>
+    </>
   );
 }
